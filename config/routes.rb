562df5a814b3064b5e2d94da56f5baf841d0d18e
Mori116 <cacaomame096@gmail.com>
2021-05-05 07:54:33 +0000

    [Add]3-3 投稿機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :blogs  # この1行だけでindexからdestroyまでの７つのルーティングが定義される
+  resources :blogs # この1行だけでindexからdestroyまでの７つのルーティングが定義される
 end
